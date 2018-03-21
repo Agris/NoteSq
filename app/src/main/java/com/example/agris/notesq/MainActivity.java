5dc62e05919dd893ca35cf9930b63feeba728918
agris <testetajsagris@gmail.com>
2018-03-21 12:58:36 +0200

    1 commits

--- app/src/main/java/com/example/agris/notesq/MainActivity.java
+++ app/src/main/java/com/example/agris/notesq/MainActivity.java
@@ -1,12 +1,14 @@
 package com.example.agris.notesq;
 
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import java.io.BufferedReader;
 import java.io.DataInputStream;
@@ -19,6 +21,7 @@ import java.io.InputStreamReader;
 public class MainActivity extends AppCompatActivity {
     public static final String DEBUGTAG = "ANE";
     public static final String TEXTFILE = "notesq.txt";
+    public static final String FILESAVED = "FileSaved";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,9 +29,15 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         addSaveButtonListener();
-        loadSavedFile ();
+        SharedPreferences prefs = getPreferences(MODE_PRIVATE);
+        boolean fileSaved = prefs.getBoolean(FILESAVED, false);
 
-    }
+        if(fileSaved) {
+            loadSavedFile();
+        }
+        }
+
+
     private void loadSavedFile(){
         try {
 
@@ -61,10 +70,18 @@ public class MainActivity extends AppCompatActivity {
                 EditText editText = (EditText) findViewById (R.id.text);
                 String text = editText.getText().toString();
 
-                try {
-                    FileOutputStream fos = openFileOutput(TEXTFILE, Context.MODE_PRIVATE);
+                try {/*
+                    FileOutputStream fos = openFileOutput(TEXTFILE,
+                            Context.MODE_PRIVATE);
                     fos.write(text.getBytes());
                     fos.close();
+
+                    SharedPreferences prefs = getPreferences(MODE_PRIVATE);
+                    SharedPreferences.Editor editor = prefs.edit();
+                    editor.putBoolean(FILESAVED, true);
+                    editor.commit();
+*/
+                    Toast.makeText(MainActivity.this,getString(R.string.toast_cant_save), Toast.LENGTH_LONG).show();
                 } catch (Exception e){
 
                     Log.d(DEBUGTAG, "Unable to save file");
