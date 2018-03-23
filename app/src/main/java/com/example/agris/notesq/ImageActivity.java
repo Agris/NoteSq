7fe445a3ecbc722f3b0234d8ad4bb1a0204a579f
agris <testetajsagris@gmail.com>
2018-03-23 10:08:23 +0200

    3 commits pirms OnTouchListener

--- app/src/main/java/com/example/agris/notesq/ImageActivity.java
+++ app/src/main/java/com/example/agris/notesq/ImageActivity.java
@@ -2,6 +2,9 @@ package com.example.agris.notesq;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.ImageView;
 
 public class ImageActivity extends AppCompatActivity {
 
@@ -9,5 +12,17 @@ public class ImageActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_image);
+
+        addTouchListener();
+    }
+    private void addTouchListener(){
+    ImageView image = (ImageView) findViewById(R.id.touch_image);
+
+   image.setOnTouchListener(new View.OnTouchListener() {
+       @Override
+       public boolean onTouch(View view, MotionEvent motionEvent) {
+           return false;
+       }
+   });
     }
 }
